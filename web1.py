979a814d737baeb02a1ab8a27af2d83eece66b2b
Eduardo Nascimento <92709734+edrhat@users.noreply.github.com>
2021-10-29 23:40:30 -0300

    Add files via upload

--- web1.py
+++ web1.py
@@ -1,7 +1,18 @@
 import os
 
-site = input("Domínio: ")
-link = ("start chrome {}/robots.txt".format(site))
-link2 = ("start chrome {}/sitemap".format(site))
-os.system(link)
-os.system(link2)
+s = input("Está utilizando qual sistema ?\nWindows[1]\nLinux[2]\n ")
+if s == "1":
+    site = input("Domínio: ")
+    link = ("start chrome {}/robots.txt".format(site))
+    link2 = ("start chrome {}/sitemap".format(site))
+    os.system(link)
+    os.system(link2)
+
+if s == "2":
+    site = input("Domínio: ")
+    link = ("firefox {}/robots.txt".format(site))
+    link2 = ("firefox {}/sitemap".format(site))
+    os.system(link)
+    os.system(link2)
+    
+
